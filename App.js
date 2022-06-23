76fa99547ed4e1074533a2b6da49ed8fdac7d2b2
Muhammadasad22 <muhammadasad2291@gmail.com>
2022-06-23 14:10:44 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import Home from './screens/Home';
+
+export class App extends Component {
+  render() {
+    return <Home />;
+  }
+}
+
+export default App;
---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+import HeaderTabs from '../components/HeaderTabs';
+import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{backgroundColor: '#eee', flex: 1}}>
+      <View style={{backgroundColor: 'white', padding: 15}}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <Categories />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
